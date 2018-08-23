fbd348d99a3e00e8a648f2a6173517513a8981cb
Ciwonie <ciwonie@gmail.com>
2018-08-23 17:11:58 -0500

    Fixed package.json and .env bugs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+require("dotenv").config();
 
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: '110960',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
     database: 'bamazon'
 });
 
